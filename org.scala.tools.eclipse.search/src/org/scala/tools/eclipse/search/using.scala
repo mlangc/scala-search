3408e9e06cad5203e4834da7e4aec49d3cc6329b
Matthias Langer <mlangc@gmx.at>
2014-11-05 14:27:11 +0100

    Be more verbose when closing a resource fails

--- org.scala.tools.eclipse.search/src/org/scala/tools/eclipse/search/using.scala
+++ org.scala.tools.eclipse.search/src/org/scala/tools/eclipse/search/using.scala
@@ -1,14 +1,18 @@
 package org.scala.tools.eclipse.search
 
 import scala.util.control.Exception._
+import org.scalaide.logging.HasLogger
 
-object using {
+object using extends HasLogger {
   def apply[T <: java.io.Closeable, R](resource: => T, handlers: Catch[R] = noCatch)(body: T => R): R = {
     handlers {
       val r = resource
-      handlers.andFinally(ignoring(classOf[Any]){ r.close() }).apply{
+      handlers.andFinally(loggingCatcher{ r.close() }).apply{
         body(r)
       }
     }
   }
+
+  def loggingCatcher: Catch[Unit] =
+    nonFatalCatch.withApply{ th => logger.debug("Error closing resource", th) }
 }
